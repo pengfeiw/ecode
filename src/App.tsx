8614f528769107601fd810e9355e173156a2f9ab
fangyan <fangyan@qunhemail.com>
2022-11-20 18:57:33 +0800

    完善 demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import ECode from "./App/index";
 
+const html = "<div>ECode is cool!</div>";
+const css = `div {
+    color: #007acc;
+    font-size: 25px;
+}`;
+
 const App = () => {
-    return <ECode style={{height: "100%"}}/>;
+    return (
+        <ECode
+            style={{ height: "100%" }}
+            html={html}
+            css={css}
+        />
+    );
 };
 
 const container = document.getElementById("root");
